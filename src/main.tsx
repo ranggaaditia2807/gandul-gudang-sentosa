481bac41071dfd0ce256e0c32a339f76c7b480dd
ranggaaditia2807 <ranggaaditiya282007@gmail.com>
2025-08-29 15:23:29 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css"; // pastikan Tailwind / CSS sudah import
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
