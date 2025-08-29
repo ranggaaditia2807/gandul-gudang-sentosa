f681041737b772a929d71ae1bcb120227fc0a2e6
ranggaaditia2807 <ranggaaditiya282007@gmail.com>
2025-08-29 15:10:47 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/gandul-gudang-sentosa/", // HARUS sesuai nama repo
+  base: "/gandul-gudang-sentosa/", // 
   server: {
     host: "::",
     port: 8080,
